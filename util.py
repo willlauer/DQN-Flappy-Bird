fb3adc65e51fed5335c79ae91447fa81539c1e25
hedgehog-bandit <wlauer@stanford.edu>
2018-08-28 10:59:50 -0500

    frameworking

--- util.py
+++ util.py
@@ -6,3 +6,45 @@ def stretch_pipe(l, up_or_down):
 	
 	if up_or_down == 'up':
 		sprite = Config. 
+
+
+
+
+'''
+List all of our constants used in the game here
+'''
+screenWidth = None
+screenHeight = None
+
+pipeWidth = None 
+
+avatarWidth = None
+avatarHeight = None
+
+screenStep = None # How many pixels to shift the screen by each time
+
+initBirdY = None # The starting position for our bird
+
+hopVelocity = None # The initial velocity applied when the bird hops
+
+
+# Treat each pixel as being one meter, to simplify physics
+
+timeStep = None # How much time passes with each step of our display
+
+
+
+'''
+Equations for our game physics calculations
+'''
+def p2(p1, v, a, t):
+	'''
+	r = r0 + vt - 1/2 at^2
+	'''
+	return p1 + (v * t) - (a * (t ** 2)) / 2
+
+def v2(v, a, t):
+	'''
+	v = v0 + at
+	'''
+	return v + (a * t)
